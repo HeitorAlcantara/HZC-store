4b06c47001644c9387214dd86265cbff265ec848
unknown <heitor.magalc@gmail.com>
2022-12-15 17:55:45 -0300

    fixing js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const menu = document.querySelector('.menu-lateral');
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo');
-    console.log(window);
+    console.log(URL);
 })
 
 const inicio = document.querySelector('.menu-lateral__link--inicio');
 const picos = document.querySelector('.menu-lateral__link--picos');
 const videos = document.querySelector('.menu-lateral__link--videos');
 
-const picosHtml = URL + "/picos.html";
-const videosHtml = URL + "/videos.html";
+const picosHtml = URL + "picos.html";
+const videosHtml = URL + "videos.html";
 
 if(picosHtml == window.location.href){
     inicio.classList.remove('menu-lateral__link--ativo');
